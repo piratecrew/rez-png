ff95f799a9cc7497a97a45e530da43fa8cba352d
Fredrik Brännbacka FL <fredrik.brannbacka@ilpvfx.com>
2017-08-20 17:45:16 +0200

    Fixed cmake

--- cmake/png.cmake
+++ cmake/png.cmake
@@ -1,5 +1,5 @@
 set(png_INCLUDE_DIRS $ENV{REZ_PNG_ROOT}/include)
-set(png_LIBRARY_DIRS $ENV{REZ_PNG_ROOT}/lib)
+set(png_LIBRARY_DIRS $ENV{REZ_PNG_ROOT}/lib64)
 if(png_STATIC)
 	set(png_LIBRARIES ${png_LIBRARY_DIRS}/libpng.a)
 else()
